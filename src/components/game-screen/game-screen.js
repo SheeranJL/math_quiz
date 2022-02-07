50f3fd96583fd8117aae22faed35a356e484c7b0
SheeranJL <lewisjsheeran@protonmail.com>
2022-02-07 21:13:01 +1100

    general logic for generating new math questions done

--- src/components/game-screen/game-screen.js
+++ src/components/game-screen/game-screen.js
@@ -1,29 +1,63 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import {appContext} from '../../context/context.js';
 
 import './game-screen.scss';
 
 const GameScreen = () => {
 
-  const [answer, setAnswer] = useState(null);
+  const {data, actions} = useContext(appContext);
+  const [firstRender, setFirstRender] = useState(true);
+  const [answer, setAnswer] = useState('');
+  const [question, setQuestion] = useState({
+    firstNumber: null,
+    operator: null,
+    secondNumber: null,
+    solution: null,
+  })
+  const {firstNumber, operator, secondNumber, solution} = question;
 
-  const handleInput = (e) => {
-    const regex = /^[0-9]+$/;
-    const input = e.target.value
+  const [correctGuess, setCorrectGuess] = useState(0);
 
-    if (regex.test(input)) {
-      setAnswer(input)
+
+  useEffect(() => {
+    //On first render, generate a new question//
+    if (firstRender) {
+      setQuestion(actions.generateEasyQuestion())
+      setFirstRender(false);
+    }
+
+    //generate a new question on each correct guess//
+    if (parseInt(solution) === parseInt(answer)) {
+      setQuestion(actions.generateEasyQuestion())
+    }
+
+  }, [correctGuess, answer])
+
+
+  function displayQuestion() {
+    if (operator === '+') {
+      return `${firstNumber} + ${secondNumber}`
+    } else if (operator === '-') {
+      return `${firstNumber} - ${secondNumber}`
+    } else if (operator === '/') {
+      return `${firstNumber} ÷ ${secondNumber}`
+    } else if (operator === '*') {
+      return `${firstNumber} x ${secondNumber}`
     }
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('text submit')
+  const handleChange = (e) => {
+
+    //regex to limit non-numerical input//
+    const re = /^[0-9\b]+$/;
+    if (e.target.value === '' || re.test(e.target.value)) {
+        setAnswer(e.target.value)
+    }
   }
+
 
   return (
     <div className='game-screen-container'>
-
       <div className='game-screen'>
 
         <div className='game-mode-title'>
@@ -31,17 +65,14 @@ const GameScreen = () => {
         </div>
 
         <div className='question'>
-          <span>3 x 2</span>
+          <span>{displayQuestion()}</span>
         </div>
 
         <div className='input-section'>
-          <form onSubmit={handleSubmit}>
-            <input type='text' onChange={handleInput} value={answer}/>
-          </form>
+            <input type='text' pattern="[0-9]*" onChange={handleChange} value={answer}/>
         </div>
 
       </div>
-
     </div>
   )
 
--- src/context/context.js
+++ src/context/context.js
@@ -7,15 +7,25 @@ const Provider = ({children}) => {
   const [gameMode, setGameMode] = useState('tester')
   const [startGame, setStartGame] = useState(false);
 
-  const handleGameMode = () => {
 
+  function generateEasyQuestion() {
+
+    const operators = ['*', '+', '-', '/']
+
+    const question = {
+      firstNumber: Math.floor(Math.random() * 11),
+      operator: operators[Math.floor(operators.length * Math.random())],
+      secondNumber: Math.floor(Math.random() * 11),
+      solution: 4,
+    }
+    return question;
   }
 
 
   return (
     <appContext.Provider value={{
         data: {startGame, gameMode},
-        actions: {setStartGame, setGameMode}
+        actions: {setStartGame, setGameMode, generateEasyQuestion}
     }}>
       {children}
     </appContext.Provider>
